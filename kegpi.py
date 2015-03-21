d04b02506c6ede12d64f2f6af0f1b866b4586b18
bradbrok <bradbrok@Brads-MacBook-Pro.local>
2015-03-21 00:51:32 -0700

    Database shenannigans, I should really stop committing at 1.00am

--- kegpi.py
+++ kegpi.py
@@ -7,6 +7,7 @@ The tastiest solution to monitor your keggerator.
 from flowmeter import *
 #from kegs import *
 import os
+import sys
 import time
 import RPi.GPIO as GPIO
 import ConfigParser
@@ -22,13 +23,17 @@ config.read("tap_config.ini")
 #Database config.
 db = sqlite3.connect('beverage_db')
 cursor = db.cursor()
-cursor.execute('''CREATE TABLE bevs_tap1(id INTEGER PRIMARY KEY, time_pour TEXT, date_pour TEXT,
-	clicks INTEGER, ml_pour NUMERIC, oz_pour NUMERIC, pour_count INTEGER)
-	''')
-cursor.execute('''CREATE TABLE bevs_tap2(id INTEGER PRIMARY KEY, time_pour TEXT, date_pour TEXT,
-	clicks INTEGER, ml_pour NUMERIC, oz_pour NUMERIC, pour_count INTEGER)
-	''')
-db.commit()
+if os.path.isfile(db):
+	pass
+else:
+	cursor.execute('''CREATE TABLE bevs_tap1(id INTEGER PRIMARY KEY, time_pour TEXT, date_pour TEXT,
+		clicks INTEGER, ml_pour NUMERIC, oz_pour NUMERIC, pour_count INTEGER)
+		''')
+	cursor.execute('''CREATE TABLE bevs_tap2(id INTEGER PRIMARY KEY, time_pour TEXT, date_pour TEXT,
+		clicks INTEGER, ml_pour NUMERIC, oz_pour NUMERIC, pour_count INTEGER)
+		''')
+	db.commit()
+db.close()
 
 #FlowMeter class.
 f = FlowMeter()
@@ -46,47 +51,50 @@ GPIO.setup(flow_pin_tap2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def to_pi_tap1(channel):
 	f.update()
 	f.calibration = float(config.get('taps', 'calibration_0'))
-	tap_no = 1
+	tap = 1
 
 def to_pi_tap2(channel):
 	f.update()
 	f.calibration = float(config.get('taps', 'calibration_1'))
-	tap_no = 2
+	tap = 2
 
 #Add the event detection to trigger callback.
 GPIO.add_event_detect(flow_pin_tap1, GPIO.RISING, callback=to_pi_tap1)
 GPIO.add_event_detect(flow_pin_tap2, GPIO.RISING, callback=to_pi_tap2)
 
 #Called when the pour event happens.
-def update_db(tap_no):
-	time_pour = f.last_pour_time
+def update_db():
+	db.connect('beverage_db')
+	time_pour = time.time()
 	date_pour = time.ctime()
 	clicks = f.last_clicks
 	ml_pour = f.to_ml
 	oz_pour = f.last_pour_oz
 	pour_count = f.drink_count
-	if (tap_no == 1):
+	if (tap == 1):
 		cursor.execute('''INSERT INTO bevs_tap1(time_pour, date_pour, clicks, ml_pour, oz_pour, pour_count)
 		VALUES (?,?,?,?,?,?)''', (time_pour, date_pour, clicks, ml_pour, oz_pour, pour_count))
 		db.commit()
-	elif (tap_no == 2):
+	elif (tap == 2):
 		cursor.execute('''INSERT INTO bevs_tap2(time_pour, date_pour, clicks, ml_pour, oz_pour, pour_count)
 		VALUES (?,?,?,?,?,?)''', (time_pour, date_pour, clicks, ml_pour, oz_pour, pour_count))
 		db.commit()
 	else:
 		print "No data for tap can be recorded. Something went wrong."
+	f.pour_event_occured = False
+	db.close()
 
 try:
  	while True:
 		time.sleep(1)
 		f.last_pour_func()
 		if f.pour_event_occured == True:
-			update_db(tap_no)
+			update_db()
 		else:
 			pass
 
 except KeyboardInterrupt:
-	print "Goodbye"
+	print "Goodbye, enjoy your delicious beverages!"
  	GPIO.cleanup()
 	db.close()
 	sys.exit()
