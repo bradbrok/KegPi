6630c7605718c46322268cf0492c29550d31dd9f
bradbrok <bradbrok@Brads-MacBook-Pro.local>
2015-03-20 19:14:07 -0700

    Tap logic

--- kegpi.py
+++ kegpi.py
@@ -10,10 +10,16 @@ import os
 import time
 import RPi.GPIO as GPIO
 import threading
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read("tap_config.ini")
 
 #This will soon update when I get around to using this app.
 beers_drank_while_coding_this = "8"
 f = FlowMeter()
+
+
 
 #Flow meter pins on GPIO based on BCM layout.
 flow_pin_tap1 = 23
@@ -25,12 +31,17 @@ GPIO.setup(flow_pin_tap1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #GPIO.setup(flow_pin_tap2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #This is the def for the callback.
-def to_pi(channel):
+def to_pi_tap1(channel):
+	f.calibration = float(config.get('taps', 'calibration_0'))
 	f.update()
 
+#def to_pi_tap2(channel):
+#	f.calibration = float(config.get('taps', 'calibration_1'))
+#	f.update()
+
 #Add the event detection to trigger callback.
-GPIO.add_event_detect(flow_pin_tap1, GPIO.RISING, callback=to_pi)
-#GPIO.add_event_detect(flow_pin_tap2, GPIO.RISING, callback=to_pi)
+GPIO.add_event_detect(flow_pin_tap1, GPIO.RISING, callback=to_pi_tap1)
+#GPIO.add_event_detect(flow_pin_tap2, GPIO.RISING, callback=to_pi_tap2)
 
 try:
  	while True:
